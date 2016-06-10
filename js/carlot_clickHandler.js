0a519eed3b0fcc17c54992af2333420189939ba5
Dave Harms <daveharmswebdev@gmail.com>
2016-06-10 11:45:38 -0500

    added carCard clickhandlers and border manipulation and textarea focus and clear

---
+++ js/carlot_clickHandler.js
@@ -0,0 +1,18 @@
+var carLot = (function(lot) {
+
+	lot.clickHandler = function(event) {
+		var el = document.getElementsByClassName('carCard');
+		for (var i = 0; i < el.length; i++) {
+			el[i].classList.remove('selected');
+		}
+		carID = event.currentTarget.id;
+		document.getElementById(carID).classList.add('selected');
+		descInput.focus();
+		descInput.value = '';
+	};
+
+	return lot;
+
+})(carLot || {});
+
+carLot.init();
